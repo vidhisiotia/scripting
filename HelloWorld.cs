21eade25e4edf81c4ede7244fe03c8c9283f1d26
vidhisiotia <157894492+vidhisiotia@users.noreply.github.com>
2024-01-27 16:47:00 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello world");
+        int number;
+        int getNumber();
+        {
+            return 3;
+        }
+        number = getNumber();
+        print(number)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
